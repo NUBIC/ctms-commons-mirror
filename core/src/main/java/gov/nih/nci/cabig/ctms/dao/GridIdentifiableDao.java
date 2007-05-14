3be65f9be3701239bbb5514be956d0c9dc0325e0
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-05-14 02:13:25 +0000

    Extend base DAO interface
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ctmscommons/trunk@21 95e2a1e8-af37-462d-93d7-7d6c60a9484c

--- core/src/main/java/gov/nih/nci/cabig/ctms/dao/GridIdentifiableDao.java
+++ core/src/main/java/gov/nih/nci/cabig/ctms/dao/GridIdentifiableDao.java
@@ -1,11 +1,12 @@
 package gov.nih.nci.cabig.ctms.dao;
 
 import gov.nih.nci.cabig.ctms.domain.GridIdentifiable;
+import gov.nih.nci.cabig.ctms.domain.DomainObject;
 
 /**
  * @author Rhett Sutphin
  */
-public interface GridIdentifiableDao<T extends GridIdentifiable> {
+public interface GridIdentifiableDao<T extends GridIdentifiable & DomainObject> extends DomainObjectDao<T> {
     /**
      * Return the existing object with the given grid ID, or null if there isn't one.
      */
