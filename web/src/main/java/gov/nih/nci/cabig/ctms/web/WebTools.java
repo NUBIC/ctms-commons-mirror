84c8fe39646c053f2870011b1ea91ba7b3135901
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-07-30 21:11:42 +0000

    Port WebUtils from core-commons
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ctmscommons/trunk@76 95e2a1e8-af37-462d-93d7-7d6c60a9484c

---
+++ web/src/main/java/gov/nih/nci/cabig/ctms/web/WebTools.java
@@ -0,0 +1,114 @@
+package gov.nih.nci.cabig.ctms.web;
+
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.beans.PropertyDescriptor;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+/**
+ * Utility methods for exposing various web context information as standard collections.
+ * This is mainly used to make them easily traversable in JSP EL.
+ *
+ * Derived from NU's core-commons library (WebUtils, there).
+ *
+ * @author Rhett Sutphin
+ */
+public class WebTools {
+    @SuppressWarnings({ "unchecked" })
+    public static SortedMap<String, Object> sessionAttributesToMap(final HttpSession session) {
+        if (session != null) {
+            return namedAttributesToMap(session.getAttributeNames(), new AttributeAccessor() {
+                public Object getAttribute(String name) { return session.getAttribute(name); }
+            });
+        } else {
+            return emptySortedMap();
+        }
+    }
+
+    @SuppressWarnings({ "unchecked" })
+    public static SortedMap<String, Object> requestAttributesToMap(final HttpServletRequest request) {
+        return namedAttributesToMap(request.getAttributeNames(), new AttributeAccessor() {
+            public Object getAttribute(String name) { return request.getAttribute(name); }
+        });
+    }
+
+    private final static Collection<String> EXCLUDED_REQUEST_PROPERTIES = Arrays.asList(
+        "class", "session", "headerNames", "attributeNames", "parameterMap", "parameterNames",
+        "reader", "inputStream", "locales", "cookies", "requestedSessionIdFromUrl"
+    );
+
+    public static SortedMap<String, Object> requestPropertiesToMap(HttpServletRequest request) {
+        BeanWrapper wrapped = new BeanWrapperImpl(request);
+        SortedMap<String, Object> map = new TreeMap<String, Object>();
+        for (PropertyDescriptor descriptor : wrapped.getPropertyDescriptors()) {
+            String name = descriptor.getName();
+            if (!EXCLUDED_REQUEST_PROPERTIES.contains(name) && descriptor.getReadMethod() != null) {
+                map.put(name, wrapped.getPropertyValue(name));
+            }
+        }
+        return map;
+    }
+
+    @SuppressWarnings({ "unchecked" })
+    public static SortedMap<String, String[]> headersToMap(final HttpServletRequest request) {
+        return namedAttributesToMap(request.getHeaderNames(), new AttributeAccessor<String[]>() {
+            @SuppressWarnings({ "unchecked" })
+            public String[] getAttribute(String headerName) {
+                Enumeration<String> values = request.getHeaders(headerName);
+                List<String> valList = new ArrayList<String>();
+                while (values.hasMoreElements()) {
+                    valList.add(values.nextElement());
+                }
+                return valList.toArray(new String[valList.size()]);
+            }
+        });
+    }
+
+    @SuppressWarnings({ "unchecked" })
+    public static SortedMap<String, Object> servletContextAttributesToMap(final ServletContext context) {
+        return namedAttributesToMap(context.getAttributeNames(), new AttributeAccessor<Object>() {
+            public Object getAttribute(String name) {
+                return context.getAttribute(name);
+            }
+        });
+    }
+
+    @SuppressWarnings({ "unchecked" })
+    public static SortedMap<String, Object> contextInitializationParametersToMap(final ServletContext context) {
+        return namedAttributesToMap(context.getInitParameterNames(), new AttributeAccessor<Object>() {
+            public Object getAttribute(String name) {
+                return context.getInitParameter(name);
+            }
+        });
+    }
+
+    private static SortedMap<String, Object> emptySortedMap() {
+        return Collections.unmodifiableSortedMap(new TreeMap<String, Object>());
+    }
+
+    private static <T> SortedMap<String, T> namedAttributesToMap(
+        Enumeration<String> names, AttributeAccessor<T> accessor
+    ) {
+        SortedMap<String, T> map = new TreeMap<String, T>();
+        while (names.hasMoreElements()) {
+            String name = names.nextElement();
+            map.put(name, accessor.getAttribute(name));
+        }
+        return Collections.unmodifiableSortedMap(map);
+    }
+
+    private static interface AttributeAccessor<T> {
+        T getAttribute(String name);
+    }
+}
---
+++ web/src/test/java/gov/nih/nci/cabig/ctms/web/WebToolsTest.java
@@ -0,0 +1,137 @@
+package gov.nih.nci.cabig.ctms.web;
+
+import junit.framework.TestCase;
+
+import java.util.SortedMap;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.Collection;
+import java.util.List;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Map;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
+
+/**
+ * @author Rhett Sutphin
+ */
+public class WebToolsTest extends TestCase {
+    private MockHttpServletRequest request;
+    private MockHttpSession session;
+    private static final Collection<String> EXPECTED_REQUEST_PROPERTIES = Arrays.asList(
+            // From HttpServletRequest
+        "authType", "contextPath", "method", "pathInfo", "pathTranslated", "queryString",
+        "remoteUser", "requestedSessionId", "requestURL", "requestURI", "servletPath",
+        "userPrincipal", "requestedSessionIdFromCookie", "requestedSessionIdFromURL",
+        "requestedSessionIdValid",
+        // From ServletRequest
+        "characterEncoding", "contentLength", "contentType", "locale", "localAddr",
+        "localName", "localPort", "protocol", "remoteAddr", "remoteHost", "remotePort",
+        "scheme", "serverName", "serverPort", "secure",
+        // From MockHttpServletRequest
+        "active"
+    );
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        request = new MockHttpServletRequest();
+        request.setServerName("www.neuromice.org");
+        request.setContextPath("/");
+
+        session = new MockHttpSession();
+    }
+
+    public void testSessionAttributesToMap() {
+        session.setAttribute("jimmy", "james");
+        session.setAttribute("johnny", "johnson");
+        session.setAttribute("wnyx", 585);
+
+        SortedMap<String, Object> map = WebTools.sessionAttributesToMap(session);
+
+        Set<String> keys = map.keySet();
+        assertContains(keys, "jimmy");
+        assertContains(keys, "johnny");
+        assertContains(keys, "wnyx");
+
+        assertEquals(map.get("jimmy"), "james");
+        assertEquals(map.get("johnny"), "johnson");
+        assertEquals(map.get("wnyx"), 585);
+
+        // test order
+        Iterator<String> keysIt = keys.iterator();
+        assertEquals("jimmy",  keysIt.next());
+        assertEquals("johnny", keysIt.next());
+        assertEquals("wnyx",   keysIt.next());
+    }
+
+    public void testRequestPropertiesToMapIsNotMissingAnything() throws Exception {
+        Map<String, Object> actual = WebTools.requestPropertiesToMap(request);
+
+        Set<String> missing = new LinkedHashSet<String>(EXPECTED_REQUEST_PROPERTIES);
+        for (String property : EXPECTED_REQUEST_PROPERTIES) {
+            if (actual.containsKey(property)) missing.remove(property);
+        }
+        assertEquals("One or more expected properties missing: " + missing, 0, missing.size());
+    }
+
+    public void testRequestPropertiesToMapHasNoExtras() throws Exception {
+        Map<String, Object> actual = WebTools.requestPropertiesToMap(request);
+
+        Set<String> extra = new LinkedHashSet<String>(actual.keySet());
+        for (String property : EXPECTED_REQUEST_PROPERTIES) {
+            if (actual.containsKey(property)) extra.remove(property);
+        }
+        assertEquals("One or more extra properties in map: "+ extra, 0, extra.size());
+    }
+
+    public void testRequestPropertiesToMapValues() throws Exception {
+        // spot checks
+        Map<String, Object> actual = WebTools.requestPropertiesToMap(request);
+        assertEquals(request.getServerName(), actual.get("serverName"));
+        assertEquals(request.getContextPath(), actual.get("contextPath"));
+    }
+
+    public void testRequestAttributesToMap() {
+        request.setAttribute("wnyx", 585);
+        request.setAttribute("jimmy", "james");
+        request.setAttribute("johnny", "johnson");
+
+        SortedMap<String, Object> map = WebTools.requestAttributesToMap(request);
+
+        Set<String> keys = map.keySet();
+        assertContains(keys, "jimmy");
+        assertContains(keys, "johnny");
+        assertContains(keys, "wnyx");
+
+        assertEquals(map.get("jimmy"), "james");
+        assertEquals(map.get("johnny"), "johnson");
+        assertEquals(map.get("wnyx"), 585);
+
+        // test order
+        Iterator<String> keysIt = keys.iterator();
+        assertEquals("jimmy",  keysIt.next());
+        assertEquals("johnny", keysIt.next());
+        assertEquals("wnyx",   keysIt.next());
+    }
+
+    public void testHeadersToMap() throws Exception {
+        request.addHeader("Content-Type", "text/plain");
+        request.addHeader("X-Doubled", "1");
+        request.addHeader("X-Doubled", "2");
+
+        Map<String, String[]> actual = WebTools.headersToMap(request);
+        assertEquals("Wrong number of entries", 2, actual.size());
+        assertEquals(actual.get("Content-Type")[0], "text/plain");
+        assertEquals(actual.get("X-Doubled")[0], "1");
+        assertEquals(actual.get("X-Doubled")[1], "2");
+    }
+
+    // TODO: until CoreTestCase is moved into ctms-commons
+    private void assertContains(Collection<?> actual, Object expected) {
+        assertTrue("Collection does not contain " + expected + ": " + actual, actual.contains(expected));
+    }
+}
