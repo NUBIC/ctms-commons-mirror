671bad09b13a5a367d5073f61546a5df755b6f2d
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-10-11 18:40:59 +0000

    BuildInfo object for capturing information about the build, including version and timestamp
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ctmscommons/trunk@102 95e2a1e8-af37-462d-93d7-7d6c60a9484c

---
+++ core/src/main/java/gov/nih/nci/cabig/ctms/tools/BuildInfo.java
@@ -0,0 +1,112 @@
+package gov.nih.nci.cabig.ctms.tools;
+
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+/**
+ * @author Rhett Sutphin
+ */
+public class BuildInfo {
+    private static final String DEFAULT_APPLICATION_NAME = "Untitled CTMS application";
+    private static final ThreadLocal<DateFormat> TIMESTAMP_FORMAT = new ThreadLocal<DateFormat>();
+
+    private String applicationName;
+    private String versionNumber;
+
+    private String username;
+    private String hostname;
+    private Timestamp timestamp;
+
+    ////// LOGIC
+
+    public String getBuildName() {
+        StringBuilder name = new StringBuilder();
+        if (getApplicationName() == null) {
+            name.append(DEFAULT_APPLICATION_NAME);
+        } else {
+            name.append(getApplicationName());
+        }
+        name.append(' ');
+        if (getVersionNumber() == null) {
+            name.append("[unknown SNAPSHOT]");
+        } else {
+            name.append("v. ").append(getVersionNumber());
+        }
+
+        if (isSnapshot()) {
+            name.append(" (");
+
+            boolean hasUser = getUsername() != null;
+            boolean hasHost = getHostname() != null;
+            if (hasUser) name.append(getUsername());
+            if (hasUser && hasHost) name.append('@');
+            if (hasHost) name.append(getHostname());
+            if (hasUser || hasHost) name.append(' ');
+
+            if (getTimestamp() == null) {
+                name.append("[unknown time]");
+            } else {
+                name.append(getTimestampFormat().format(getTimestamp()));
+            }
+            name.append(')');
+        }
+
+        return name.toString();
+    }
+
+    protected DateFormat getTimestampFormat() {
+        DateFormat format = TIMESTAMP_FORMAT.get();
+        if (format == null) {
+            format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            TIMESTAMP_FORMAT.set(format);
+        }
+        return format;
+    }
+
+    public boolean isSnapshot() {
+        return versionNumber == null || versionNumber.toUpperCase().endsWith("SNAPSHOT");
+    }
+
+    ////// BEAN PROPERTIES
+
+    public String getApplicationName() {
+        return applicationName;
+    }
+
+    public void setApplicationName(String applicationName) {
+        this.applicationName = applicationName;
+    }
+
+    public String getVersionNumber() {
+        return versionNumber;
+    }
+
+    public void setVersionNumber(String versionNumber) {
+        this.versionNumber = versionNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+}
---
+++ core/src/test/java/gov/nih/nci/cabig/ctms/tools/BuildInfoTest.java
@@ -0,0 +1,78 @@
+package gov.nih.nci.cabig.ctms.tools;
+
+import junit.framework.TestCase;
+
+import java.util.Calendar;
+
+import gov.nih.nci.cabig.ctms.lang.DateTools;
+
+/**
+ * @author Rhett Sutphin
+ */
+public class BuildInfoTest extends TestCase {
+    private BuildInfo buildInfo = new BuildInfo();
+    
+    public void testIsSnapshotWhenItIs() throws Exception {
+        buildInfo.setVersionNumber("0.7-SNAPSHOT");
+        assertTrue(buildInfo.isSnapshot());
+    }
+    
+    public void testIsSnapshotWhenItIsNot() throws Exception {
+        buildInfo.setVersionNumber("0.7");
+        assertFalse(buildInfo.isSnapshot());
+    }
+    
+    public void testIsSnapshotWhenNullVersion() throws Exception {
+        buildInfo.setVersionNumber(null);
+        assertTrue(buildInfo.isSnapshot());
+    }
+    
+    private void addAllFields() {
+        buildInfo.setApplicationName("CTMS-o");
+        buildInfo.setHostname("localhost");
+        buildInfo.setVersionNumber("1.22-SNAPSHOT");
+        buildInfo.setUsername("builder");
+        buildInfo.setTimestamp(DateTools.createTimestamp(2007, Calendar.FEBRUARY, 5, 13, 18, 8));
+    }
+
+    public void testReleaseBuildName() throws Exception {
+        addAllFields();
+        buildInfo.setVersionNumber("1.28");
+        assertEquals("CTMS-o v. 1.28", buildInfo.getBuildName());
+    }
+
+    public void testSnapshotBuildName() throws Exception {
+        addAllFields();
+        assertEquals("CTMS-o v. 1.22-SNAPSHOT (builder@localhost 2007-02-05 13:18:08)", buildInfo.getBuildName());
+    }
+
+    public void testSnapshotBuildNameNoAppName() throws Exception {
+        addAllFields();
+        buildInfo.setApplicationName(null);
+        assertEquals("Untitled CTMS application v. 1.22-SNAPSHOT (builder@localhost 2007-02-05 13:18:08)", buildInfo.getBuildName());
+    }
+
+    public void testSnapshotBuildNameNoVersion() throws Exception {
+        addAllFields();
+        buildInfo.setVersionNumber(null);
+        assertEquals("CTMS-o [unknown SNAPSHOT] (builder@localhost 2007-02-05 13:18:08)", buildInfo.getBuildName());
+    }
+
+    public void testSnapshotBuildNameNoUser() throws Exception {
+        addAllFields();
+        buildInfo.setUsername(null);
+        assertEquals("CTMS-o v. 1.22-SNAPSHOT (localhost 2007-02-05 13:18:08)", buildInfo.getBuildName());
+    }
+
+    public void testSnapshotBuildNameNoHostname() throws Exception {
+        addAllFields();
+        buildInfo.setHostname(null);
+        assertEquals("CTMS-o v. 1.22-SNAPSHOT (builder 2007-02-05 13:18:08)", buildInfo.getBuildName());
+    }
+
+    public void testSnapshotBuildNameNoTimestamp() throws Exception {
+        addAllFields();
+        buildInfo.setTimestamp(null);
+        assertEquals("CTMS-o v. 1.22-SNAPSHOT (builder@localhost [unknown time])", buildInfo.getBuildName());
+    }
+}
