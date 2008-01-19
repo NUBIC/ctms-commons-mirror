ce8d4ba4450cc6ab0d45d86d297c9f2babb5c5d7
Rhett Sutphin <r-sutphin@northwestern.edu>
2008-01-19 01:14:34 +0000

    Correct CSMAuthenticationProvider to follow the contract of its base class with regards to authenticating cached users
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ctmscommons/trunk@130 95e2a1e8-af37-462d-93d7-7d6c60a9484c

--- acegi/acegi-csm/src/main/java/gov/nih/nci/security/acegi/csm/authentication/CSMAuthenticationProvider.java
+++ acegi/acegi-csm/src/main/java/gov/nih/nci/security/acegi/csm/authentication/CSMAuthenticationProvider.java
@@ -10,64 +10,60 @@ import org.acegisecurity.BadCredentialsException;
 import org.acegisecurity.InsufficientAuthenticationException;
 import org.acegisecurity.providers.UsernamePasswordAuthenticationToken;
 import org.acegisecurity.providers.dao.AbstractUserDetailsAuthenticationProvider;
+import org.acegisecurity.providers.dao.UserCache;
 import org.acegisecurity.userdetails.UserDetails;
 import org.acegisecurity.userdetails.UserDetailsService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-public class CSMAuthenticationProvider extends
-		AbstractUserDetailsAuthenticationProvider {
-	
-	private static Log logger = LogFactory.getLog(CSMAuthenticationProvider.class);
-	
-	private AuthenticationManager csmAuthenticationManager;
-	private UserDetailsService userDetailsService;
-	
+public class CSMAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
+    private Log log = LogFactory.getLog(getClass());
 
-	@Override
-	protected void additionalAuthenticationChecks(UserDetails userName,
-			UsernamePasswordAuthenticationToken token)
-			throws AuthenticationException {
-		//Nothing to do here since authentication happens in retrieveUser
-	}
+    private AuthenticationManager csmAuthenticationManager;
+    private UserDetailsService userDetailsService;
 
-	@Override
-	protected UserDetails retrieveUser(String userName,
-			UsernamePasswordAuthenticationToken token)
-			throws AuthenticationException {
-		logger.debug("Authenticating " + userName + "...");
-		try{
-			getCsmAuthenticationManager().authenticate(userName, (String)token.getCredentials());
-		}catch(CSInsufficientAttributesException ex){
-			logger.debug("authentication failed: insufficient attributes", ex);
-			throw new InsufficientAuthenticationException(ex.getMessage(), ex);
-		}catch(CSLoginException ex){
-			logger.debug("authentication failed: login exception", ex);
-			throw new BadCredentialsException(ex.getMessage(), ex);
-		}catch(Exception ex){
-			logger.debug("authentication failed: unknown", ex);
-			throw new AuthenticationServiceException("Error authenticating: " + ex.getMessage(), ex);
-		}
-		logger.debug("authentication succeeded");
+    @Override
+    protected void additionalAuthenticationChecks(
+        UserDetails user, UsernamePasswordAuthenticationToken token
+    ) throws AuthenticationException {
+        log.debug("Authenticating " + user.getUsername() + "...");
+        try {
+            getCsmAuthenticationManager().authenticate(user.getUsername(), (String) token.getCredentials());
+        } catch (CSInsufficientAttributesException ex) {
+            log.debug("authentication failed: insufficient attributes", ex);
+            throw new InsufficientAuthenticationException(ex.getMessage(), ex);
+        } catch (CSLoginException ex) {
+            log.debug("authentication failed: login exception", ex);
+            throw new BadCredentialsException(ex.getMessage(), ex);
+        } catch (Exception ex) {
+            log.debug("authentication failed: unknown", ex);
+            throw new AuthenticationServiceException("Error authenticating: " + ex.getMessage(), ex);
+        }
+        log.debug("authentication succeeded");
+    }
 
-		return getUserDetailsService().loadUserByUsername(userName);
-	}
+    @Override
+    protected UserDetails retrieveUser(
+        String userName, UsernamePasswordAuthenticationToken token
+    ) throws AuthenticationException {
+        return getUserDetailsService().loadUserByUsername(userName);
+    }
 
-	public AuthenticationManager getCsmAuthenticationManager() {
-		return csmAuthenticationManager;
-	}
+    public AuthenticationManager getCsmAuthenticationManager() {
+        return csmAuthenticationManager;
+    }
 
-	public void setCsmAuthenticationManager(
-			AuthenticationManager csmAuthenticationManager) {
-		this.csmAuthenticationManager = csmAuthenticationManager;
-	}
+    public void setCsmAuthenticationManager(
+        AuthenticationManager csmAuthenticationManager) {
+        this.csmAuthenticationManager = csmAuthenticationManager;
+    }
 
-	public UserDetailsService getUserDetailsService() {
-		return userDetailsService;
-	}
+    public UserDetailsService getUserDetailsService() {
+        return userDetailsService;
+    }
 
-	public void setUserDetailsService(UserDetailsService userDetailsService) {
-		this.userDetailsService = userDetailsService;
-	}
+    public void setUserDetailsService(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
 }
