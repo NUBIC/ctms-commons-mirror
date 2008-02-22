60ac5a1e77a8cd2d936d5b43320f3304c81400c8
Rhett Sutphin <r-sutphin@northwestern.edu>
2008-02-22 16:15:40 +0000

    Log actions, if the user provides a target
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ctmscommons/trunk@159 95e2a1e8-af37-462d-93d7-7d6c60a9484c

--- testing-all/testing/src/main/java/gov/nih/nci/cabig/ctms/testing/MockRegistry.java
+++ testing-all/testing/src/main/java/gov/nih/nci/cabig/ctms/testing/MockRegistry.java
@@ -1,6 +1,7 @@
 package gov.nih.nci.cabig.ctms.testing;
 
 import static org.easymock.classextension.EasyMock.*;
+import org.apache.commons.logging.Log;
 
 import java.util.Set;
 import java.util.LinkedHashSet;
@@ -10,9 +11,15 @@ import java.lang.reflect.Method;
  * @author Rhett Sutphin
  */
 public class MockRegistry {
+    private Log log;
     private Set<Object> mocks;
 
     public MockRegistry() {
+        this(null);
+    }
+
+    public MockRegistry(Log log) {
+        this.log = log;
         mocks = new LinkedHashSet<Object>();
     }
 
@@ -25,19 +32,32 @@ public class MockRegistry {
     }
 
     public void replayMocks() {
-        for (Object mock : mocks) replay(mock);
+        for (Object mock : mocks) {
+            debug("Replaying %s", mock);
+            replay(mock);
+        }
     }
 
     public void verifyMocks() {
-        for (Object mock : mocks) verify(mock);
+        for (Object mock : mocks) {
+            debug("Verifying %s", mock);
+            verify(mock);
+        }
     }
 
     public void resetMocks() {
-        for (Object mock : mocks) reset(mock);
+        for (Object mock : mocks) {
+            debug("Resetting %s", mock);
+            reset(mock);
+        }
     }
 
     private <T> T registered(T mock) {
         mocks.add(mock);
         return mock;
+    }
+
+    private void debug(String msg, Object... params) {
+        if (log != null) log.debug(String.format(msg, params));
     }
 }
