37c4e5d6193b421680813d39995ae115807f60ab
Rhett Sutphin <r-sutphin@northwestern.edu>
2010-06-14 19:28:09 +0000

    Whitespace
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ctmscommons/trunk@330 95e2a1e8-af37-462d-93d7-7d6c60a9484c

--- core/src/main/java/gov/nih/nci/cabig/ctms/audit/util/AuditUtil.java
+++ core/src/main/java/gov/nih/nci/cabig/ctms/audit/util/AuditUtil.java
@@ -3,34 +3,33 @@ package gov.nih.nci.cabig.ctms.audit.util;
 import java.lang.reflect.Method;
 
 public class AuditUtil {
-	/**
-	 * Gets the id of the persisted object
-	 * 
-	 * @param entity the object to get the id from
-	 * @return object Id or null if either id is null or entity does not
-	 * implement Integer getId() method.
-	 */
-	public static Integer getObjectId(final Object entity) {
+    /**
+     * Gets the id of the persisted object
+     *
+     * @param entity the object to get the id from
+     * @return object Id or null if either id is null or entity does not
+     *         implement Integer getId() method.
+     */
+    public static Integer getObjectId(final Object entity) {
 
-		Class objectClass = entity.getClass();
-		Method[] methods = objectClass.getMethods();
+        Class objectClass = entity.getClass();
+        Method[] methods = objectClass.getMethods();
 
-		// FIXME:Saurabh make sure it works for the Long also
-		Integer persistedObjectId = null;
-		for (Method element : methods) {
-			// If the method name equals 'getId' then invoke it to get the id of
-			// the object.
-			if (element.getName().equals("getId")) {
-				try {
-                                    persistedObjectId = (Integer) element.invoke(entity, (Object) null);
-					break;
-				}
-				catch (Exception e) {
-					return null;
-				}
-			}
-		}
-		return persistedObjectId;
-	}
-
+        // FIXME:Saurabh make sure it works for the Long also
+        Integer persistedObjectId = null;
+        for (Method element : methods) {
+            // If the method name equals 'getId' then invoke it to get the id of
+            // the object.
+            if (element.getName().equals("getId")) {
+                try {
+                    persistedObjectId = (Integer) element.invoke(entity, (Object) null);
+                    break;
+                }
+                catch (Exception e) {
+                    return null;
+                }
+            }
+        }
+        return persistedObjectId;
+    }
 }
