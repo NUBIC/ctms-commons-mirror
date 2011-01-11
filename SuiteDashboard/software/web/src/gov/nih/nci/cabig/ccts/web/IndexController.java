3821b848a7e437420060d5a4998b82858492072b
Ion Olaru <ion.olaru@semanticbits.com>
2011-01-11 16:14:56 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/ctmscommons/trunk@525 95e2a1e8-af37-462d-93d7-7d6c60a9484c

--- SuiteDashboard/software/web/src/gov/nih/nci/cabig/ccts/web/IndexController.java
+++ SuiteDashboard/software/web/src/gov/nih/nci/cabig/ccts/web/IndexController.java
@@ -1,16 +1,10 @@
 package gov.nih.nci.cabig.ccts.web;
 
-import gov.nih.nci.cabig.ccts.dao.UserDao;
-import gov.nih.nci.cabig.ccts.security.SecurityUtils;
-import gov.nih.nci.cabig.ccts.security.WebSSOUser;
-import org.acegisecurity.Authentication;
-import org.acegisecurity.context.SecurityContextHolder;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.InputStream;
-import java.util.Properties;
 
 public class IndexController extends AbstractController {
 
